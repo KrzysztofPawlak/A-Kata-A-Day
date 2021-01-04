202ff37afb703fde7a40aaa1cdb34ddc263ecb6c
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2021-01-04 23:02:05 +0100

    added description to my first kata

--- src/main/java/DRoot.java
+++ src/main/java/DRoot.java
@@ -1,6 +1,10 @@
 // Sum of Digits / Digital Root
 // 6 kyu
 
+// Given n, take the sum of the digits of n.
+// If that value has more than one digit, continue reducing in this way until a single-digit number is produced.
+// The input will be a non-negative integer.
+
 public class DRoot {
     public static int digital_root(int n) {
         int digit = n;
